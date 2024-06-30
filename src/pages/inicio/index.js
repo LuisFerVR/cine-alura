443d2e705f0c914461d3385808a8e35137416612
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-06-30 13:03:45 -0600

    Creación de componente card y  construcción de una card estática

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from "../../components/Banner";
 import Cabecera from "../../components/Cabecera/Cabecera";
+import Card from "../../components/Card";
 import Titulo from "../../components/Titulo";
 
 function Inicio (){
@@ -7,6 +8,7 @@ function Inicio (){
         <Cabecera/>
         <Banner img="home" color="#15458"/>
         <Titulo><h1>Un lugar para guardar tus videos favoritos</h1></Titulo>
+        <Card id="1" titulo="Gato" capa="https://i.pinimg.com/originals/62/43/78/624378594b7dcbaf03c5b98a358ad32e.jpg"/>
     </>);
 }
 export default Inicio;
