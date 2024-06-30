2e445c9bd884131aed682a34e2d39c8f122780e5
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-06-30 14:03:50 -0600

    Se generó la ruta de favoritos ,se compartió el header con todas las rutas, se creó la estructuctura del componente de favoritos.

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,12 +1,10 @@
 import Banner from "../../components/Banner";
-import Cabecera from "../../components/Cabecera/Cabecera";
 import Card from "../../components/Card";
 import Titulo from "../../components/Titulo";
 import styles from "./index.module.css"
 import videos from "../../data/db.json"
 function Inicio (){
     return (<>
-        <Cabecera/>
         <Banner img="home" color="#15458"/>
         <Titulo><h1>Un lugar para guardar tus videos favoritos</h1></Titulo>
         <section className={styles.container}>
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./pages/inicio";
-
+import Favoritos from "./pages/Favoritos";
+import Cabecera from "./components/Cabecera/Cabecera";
+import Container from "components/Container";
 function AppRoutes(){
     return(
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio/>}></Route>
-            </Routes>
+            <Cabecera/>
+            <Container>
+                <Routes>
+                    <Route path="/" element={<Inicio/>}></Route>
+                    <Route path="/favoritos" element={Favoritos}/>
+                </Routes>
+            </Container>
         </BrowserRouter>
     );
 }
