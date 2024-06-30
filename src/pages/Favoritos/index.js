726becd0f5573d82bb51fc00f01eba2f54407004
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-06-30 17:04:17 -0600

    Se creo un contexto para los Favoritos y se renderizaron en el componente o página de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,19 @@
+import Titulo from "../../components/Titulo/index";
 import styles from "./Favoritos.module.css"
+import Card from "../../components/Card/index";
+import { useFavoritosContext } from "../../components/context/Favoritos";
+import Banner from "../../components/Banner/index";
 
 function Favoritos(){
+    const {favorito}=useFavoritosContext();
     return(<>
-    
+    <Banner img="favorite" color="#AF7EAA"/>
+    <Titulo><h1>Mis favoritos</h1></Titulo>
+    <section className={styles.container}>
+        {favorito.map(fav=>{
+            return <Card {...fav} key={fav.id}/>
+        })}
+    </section>
     </>);
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,23 @@ import Favoritos from "./pages/Favoritos";
 import Cabecera from "./components/Cabecera/Cabecera";
 import Container from "./components/Container";
 import Pie from "./components/Footer";
+import FavoritoProvider from "./components/context/Favoritos";
+
 function AppRoutes(){
     return(
         <BrowserRouter>
             <Cabecera/>
             <Container>
-                <Routes>
-                    <Route path="/" element={<Inicio/>}></Route>
-                    <Route path="/favoritos" element={Favoritos}/>
-                </Routes>
+                <FavoritoProvider>
+                    <Routes>
+                        <Route path="/" element={<Inicio />}></Route>
+                        <Route path="/favoritos" element={<Favoritos />}></Route>
+                    </Routes>
+                </FavoritoProvider>
             </Container>
             <Pie>Hecho por Luis Fernando Vergara Ruiz</Pie>
         </BrowserRouter>
     );
 }
 
-export default AppRoutes
+export default AppRoutes;
