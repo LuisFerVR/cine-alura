2ab1d997be440246e2194803e3ba4dd18ec2d601
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-06-30 13:36:05 -0600

    Cards de video realizada con la db.json

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -2,13 +2,18 @@ import Banner from "../../components/Banner";
 import Cabecera from "../../components/Cabecera/Cabecera";
 import Card from "../../components/Card";
 import Titulo from "../../components/Titulo";
-
+import styles from "./index.module.css"
+import videos from "../../data/db.json"
 function Inicio (){
     return (<>
         <Cabecera/>
         <Banner img="home" color="#15458"/>
         <Titulo><h1>Un lugar para guardar tus videos favoritos</h1></Titulo>
-        <Card id="1" titulo="Gato" capa="https://i.pinimg.com/originals/62/43/78/624378594b7dcbaf03c5b98a358ad32e.jpg"/>
+        <section className={styles.container}>
+            {videos.map((video)=>{
+                return <Card {...video} key={video.id}/>
+            })}
+        </section>
     </>);
 }
 export default Inicio;
