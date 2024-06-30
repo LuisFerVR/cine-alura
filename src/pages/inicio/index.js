6fddd0084fed206f9cdf121df449ff1a7f5743fb
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-06-30 12:49:11 -0600

    Componente título creado

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,10 +1,12 @@
 import Banner from "../../components/Banner";
 import Cabecera from "../../components/Cabecera/Cabecera";
+import Titulo from "../../components/Titulo";
 
 function Inicio (){
     return (<>
         <Cabecera/>
         <Banner img="home" color="#15458"/>
+        <Titulo><h1>Un lugar para guardar tus videos favoritos</h1></Titulo>
     </>);
 }
 export default Inicio;
