e2fee2384b3ecab1d35590141affe9021b26ea77
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-07-06 18:38:05 -0600

    Consumir api desde un repositorio d egithub medinate json server, en código creando el estado de videos y con useEffect para el consumo tal cual del servicio.

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Titulo from "../../components/Titulo";
 import styles from "./index.module.css"
-import videos from "../../data/db.json"
 function Inicio (){
+    const [videos,setVideos] = useState([]);
+    useEffect(()=>{
+        fetch("https://my-json-server.typicode.com/LuisFerVR/alura-cinema-api/videos")
+        .then(response=>response.json())
+        .then(data=>{
+            setVideos(data)
+        })
+    },[]) 
     return (<>
         <Banner img="home" color="#15458"/>
         <Titulo><h1>Un lugar para guardar tus videos favoritos</h1></Titulo>
